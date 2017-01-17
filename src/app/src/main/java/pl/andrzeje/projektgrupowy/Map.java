cb2e5e116c0847a6564db9196802feae12da5b3f
klakier14 <darex412@o2.pl>
2017-01-17 15:50:56 +0100

    Dodanie wstepnej mapy.

--- src/app/src/main/java/pl/andrzeje/projektgrupowy/Map.java
+++ src/app/src/main/java/pl/andrzeje/projektgrupowy/Map.java
@@ -1,17 +1,36 @@
 package pl.andrzeje.projektgrupowy;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Map extends AppCompatActivity {
+public class Map extends FragmentActivity implements OnMapReadyCallback {
 
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        /*
-
-        blablabatsatsa
-         */
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng TutorialsPoint = new LatLng(21, 57);
+        mMap.addMarker(new
+                MarkerOptions().position(TutorialsPoint).title("Tutorialspoint.com"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(TutorialsPoint));
     }
 }
